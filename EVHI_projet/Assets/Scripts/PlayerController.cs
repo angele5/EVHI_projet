43d24c93afca795fed7f675d0002c979f5a7afdc
angele5 <angele.ramauge@dauphine.eu>
2025-01-01 15:41:34 +0100

    fix deplacement

--- EVHI_projet/Assets/Scripts/PlayerController.cs
+++ EVHI_projet/Assets/Scripts/PlayerController.cs
@@ -4,30 +4,66 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public float speed = 5f; // Vitesse de déplacement
-    public float speed_rotation = 100f; // Vitesse de rotation
-    public float acceleration = 5f;  // vitesse du coup de pagaie
-    public float max_speed = 10f;     // Vitesse maximale du kayak
-    public float deceleration = 2f;  // lente réduction de la vitesse / courant
+    public float acceleration = 3f; // Force du coup de pagaie
+    public float max_speed = 10f; // Vitesse maximale
+    public float rotationForce = 0.4f; // Force de rotation par coup de pagaie
+    public float resistance = 0.98f; // Résistance de l'eau, réduit la vitesse progressivement
+
     private Rigidbody2D rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        handle_input();
+    }
 
-     float horizontalInput = Input.GetAxis("Horizontal");
+    private void FixedUpdate()
+    {
+        apply_resistance();
+    }
 
-    // avancer
-    transform.Translate(Vector3.up * speed * Time.deltaTime);
+    void handle_input()
+    {
+        // Coup de pagaie droite
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            paddle(Vector2.left);
+        }
 
-    // Rotation progressive
-    float targetRotation = horizontalInput * speed_rotation;
-    transform.Rotate(Vector3.forward, Mathf.Lerp(0, targetRotation, Time.deltaTime));
-   
+        // Coup de pagaie à gauche
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            paddle(Vector2.right);
+        }
+    }
+
+    void paddle(Vector2 direction)
+    {
+        float rotation;
+        // Applique avance
+        if (rb.velocity.magnitude < max_speed)
+        {
+            rb.AddForce(transform.up * acceleration, ForceMode2D.Impulse);
+        }
+
+        // Applique rotation
+        if (direction == Vector2.right){
+            rotation =  -rotationForce;
+        }
+        else{
+            rotation = rotationForce;
+        }
+        rb.AddTorque(rotation, ForceMode2D.Impulse);
+    }
+
+    void apply_resistance()
+    {
+        // reduit vitesse lineaire et angulaire pour simuler la résistance de l'eau
+        rb.velocity *= resistance;
+        rb.angularVelocity *= resistance;
     }
 }
