269abd6f7e45abd6bda019e9a3c7674e7056acd9
angele5 <angele.ramauge@dauphine.eu>
2025-01-02 15:14:39 +0100

    menuscene

---
+++ EVHI_projet/Assets/Scripts/MenuController.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        // Charger la scène du jeu
+       // SceneManager.LoadScene("Calibration");
+       SceneManager.LoadScene("Game");
+    }
+}
